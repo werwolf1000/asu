ffd365e13afd3066855d3430aef3170cb7020bad
werwolf1000 <werwolf100@yandex.ru>
2019-03-11 23:43:44 +0300

    test

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,7 +39,7 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-   /*         ['label' => 'Home', 'url' => ['/site/index']],
+   /*      test   ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],*/
             Yii::$app->user->isGuest ? (
